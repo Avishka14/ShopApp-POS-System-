509a803368f759953c584fb739105c6713d65ad6
Avishka <Avishka@LAPTOP-TO9EP6KL>
2024-09-15 03:18:05 -0700

    Merge origin/master
    # Conflicts:
    #	src/model/MySQL.java

--- src/gui/Home.java
+++ src/gui/Home.java
@@ -15,8 +15,6 @@ public class Home extends javax.swing.JFrame {
        
     }
 
-        
-   
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
